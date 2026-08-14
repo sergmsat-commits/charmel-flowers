89763ae52ec7bb1eb93a39e7e27530651e23118f
sergmsat <sergmsat@gmail.com>
2026-08-14 11:04:15 +0200

    Enable indexing, add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://charmelflowers.pl',
+  integrations: [sitemap()],
   i18n: {
     locales: ['pl', 'ru', 'en'],
     defaultLocale: 'pl',
